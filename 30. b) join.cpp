1dc26e4c134480fc48019e3b704b427018fc2e70
192210086 <141109929+192210086@users.noreply.github.com>
2023-10-18 18:44:43 +0530

    Add files via upload

---
+++ 30. b) join.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void *my_thread(void *arg) {
+  int i;
+  for (i = 0; i < 10; i++) {
+    printf("Hello from thread!\n");
+  }
+  return NULL;
+}
+
+int main() {
+  pthread_t thread;
+  int rc;
+
+  rc = pthread_create(&thread, NULL, my_thread, NULL);
+  if (rc != 0) {
+    printf("Error creating thread!\n");
+    return 1;
+  }
+
+  pthread_join(thread, NULL);
+  return 0;
+}
